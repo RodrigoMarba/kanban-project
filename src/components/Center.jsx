30ffc3c47d3063af4a77aafb04113bef1459d3fe
Rodrigo Marba <marbarodrigo@gmail.com>
2023-08-01 17:23:23 -0300

    added NewColumn functionality

--- src/components/Center.jsx
+++ src/components/Center.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import SideBar from "./Sidebar";
 import Column from "./Column";
+import EmptyBoard from "./EmptyBoard";
+import AddEditBoardModal from "./AddEditBoardModal";
 
 function Center({ boardModalOpen, setBoardModalOpen }) {
 	const [windowSize, setWindowSize] = useState([window.innerWidth, window.innerHeight]);
@@ -32,13 +34,34 @@ function Center({ boardModalOpen, setBoardModalOpen }) {
 					: "bg-[#f4f7fd] scrollbar-hide h-screen flex dark:bg-[#20212c] overflow-x-scroll gap-6"
 			}
 		>
-			{/* {windowSize[0] >= 768 && <SideBar />} */}
+			{windowSize[0] >= 768 && <SideBar />}
 
 			{/* Columns */}
 
-			{columns.map((col, index) => (
-				<Column key={index} colIndex={index} />
-			))}
+			{columns.length > 0 ? (
+				<>
+					{columns.map((col, index) => (
+						<Column key={index} colIndex={index} />
+					))}
+
+					<div
+						onClick={() => {
+							setBoardModalOpen(true);
+						}}
+						className="h-screen dark:bg-[#2b2c3740] flex justify-center items-center font-bold text-2xl hover:text-[#635fc7] transition duration-200 cursor-pointer bg-[#e9effa] scrollbar-hide mb-2 mx-5 pt-[90px] min-w-[280px] text-[#828fa3] mt-[135px] rounded-lg"
+					>
+						+ New column
+					</div>
+				</>
+			) : (
+				<>
+					<EmptyBoard type="edit" />
+				</>
+			)}
+
+			{boardModalOpen && (
+				<AddEditBoardModal type="edit" setBoardModalOpen={setBoardModalOpen} />
+			)}
 		</div>
 	);
 }
--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -31,10 +31,10 @@ function Column({ colIndex }) {
 		<div className="scrollbar-hide mx-5 pt-[90px] min-w-[280px] ">
 			<p className=" font-semibold flex items-center gap-2 tracking-widest md:tracking-[.2em] text-[#828fa3]">
 				<div className={`rounded-full w-4 h-4 ${color} `} />
-				{col.name} ({col.tasks.length})
+				{col.name} ({col?.tasks?.length > 0 ? col?.tasks?.length : "0"})
 			</p>
 
-			{col.tasks.map((task, index) => (
+			{col.tasks?.map((task, index) => (
 				<Task key={index} taskIndex={index} colIndex={colIndex} />
 			))}
 		</div>
